d8f28861364ee2e194358686a7396e6988f07f48
Garima Singh <igarimasingh@gmail.com>
2013-07-30 14:43:05 +0100

    Added test around CLI#execute
    Refactored some existing tests and added a test around number of
    arguments for CLI class

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,99 +7,124 @@ MOCK_TEMPLATE = {
   :catalog_id   => 'default_catalog_id',
 }.freeze
 
+DEFAULTS = {
+  :debug       => false,
+  :log_level   => 5,
+  :memory      => 4096,
+  :num_cores   => 4,
+  :num_servers => 1,
+  :platform    => "production",
+  :role        => "client",
+  :ssh_config  => true,         # if not specified, use system defaults
+}
+
 describe Gds::Provisioner::CLI do
-
-  it 'should set options from built-in defaults' do
-    res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE)
-    res.should include(:role)
+  describe "#defaults" do
+    Gds::Provisioner::CLI.defaults.should ==  DEFAULTS
   end
 
-  it 'should set options from the vCloud config defaults' do
-    config = { :default => { :ip => '5.6.7.8' } }
-    res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
-    res.should include(:ip => '5.6.7.8')
+  describe "#process" do
+    it 'should set options from built-in defaults' do
+      res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE)
+      res.should include(DEFAULTS)
+    end
+
+    it 'should set options from the vCloud config defaults' do
+      config = { :default => { :ip => '5.6.7.8' } }
+      res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
+      res.should include(:ip => '5.6.7.8')
+    end
+
+    it 'should set options from the vCloud config based on the specified zone' do
+      config = { :foo => { :ip => '9.8.7.6' } }
+      res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
+      res.should include(:ip => '9.8.7.6')
+    end
+
+    it 'should set options from the machine template' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl[:ip] = '1.2.3.4'
+      res = Gds::Provisioner::CLI.process({}, tpl)
+      res.should include(:ip => '1.2.3.4')
+    end
+
+    it 'should set options from the command line' do
+      opts = {:ssh_user => 'donald'}
+      res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE, opts)
+      res.should include(:ssh_user => 'donald')
+    end
+
+    it 'should override vCloud config defaults with vCloud zone defaults' do
+      config = {
+        :default => { :ip => '1.2.3.4' },
+        :foo => { :ip => '5.6.7.8' }
+      }
+      res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
+      res.should include(:ip => '5.6.7.8')
+    end
+
+    it 'should override vCloud zone defaults with machine template options' do
+      config = { :foo => { :ip => '1.2.3.4' } }
+      tpl = MOCK_TEMPLATE.dup
+      tpl[:ip] = '5.6.7.8'
+      res = Gds::Provisioner::CLI.process(config, tpl)
+      res.should include(:ip => '5.6.7.8')
+    end
+
+    it 'should override machine template options with command line options' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl[:ssh_user] = 'aaron'
+      opts = { :ssh_user => 'binky' }
+      res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE, opts)
+      res.should include(:ssh_user => 'binky')
+    end
+
+    it 'should use catalog_id if present' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl.merge!(
+        :catalog_id => 'wibble',
+        :catalog_items => { :my_cool_template => 'incorrect' },
+        :template_name => 'my_cool_template'
+      )
+      res = Gds::Provisioner::CLI.process({}, tpl)
+      res.should include(:catalog_id => 'wibble')
+    end
+
+    it 'should use template_name to determine catalog_id if catalog_id not present' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl.delete(:catalog_id)
+      tpl.merge!(
+        :catalog_items => { :my_cool_template => 'https://correct_catalog_id' },
+        :template_name => 'my_cool_template'
+      )
+      res = Gds::Provisioner::CLI.process({}, tpl)
+      res.should include(:catalog_id => 'https://correct_catalog_id')
+    end
+
+    it 'should require a zone to be set' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl.delete(:zone)
+      expect do
+        Gds::Provisioner::CLI.process({}, tpl)
+      end.to raise_error(Gds::Provisioner::ConfigurationError, /zone/)
+    end
+
+    it 'should require a puppetmaster to be set' do
+      tpl = MOCK_TEMPLATE.dup
+      tpl.delete(:puppetmaster)
+      expect do
+        Gds::Provisioner::CLI.process({}, tpl)
+      end.to raise_error(Gds::Provisioner::ConfigurationError, /puppetmaster/)
+    end
   end
 
-  it 'should set options from the vCloud config based on the specified zone' do
-    config = { :foo => { :ip => '9.8.7.6' } }
-    res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
-    res.should include(:ip => '9.8.7.6')
-  end
+  describe "#execute" do
+    before :each do silence_output end
+    after :each do silence_output end
 
-  it 'should set options from the machine template' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl[:ip] = '1.2.3.4'
-    res = Gds::Provisioner::CLI.process({}, tpl)
-    res.should include(:ip => '1.2.3.4')
-  end
-
-  it 'should set options from the command line' do
-    opts = {:ssh_user => 'donald'}
-    res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE, opts)
-    res.should include(:ssh_user => 'donald')
-  end
-
-  it 'should override vCloud config defaults with vCloud zone defaults' do
-    config = {
-      :default => { :ip => '1.2.3.4' },
-      :foo => { :ip => '5.6.7.8' }
-    }
-    res = Gds::Provisioner::CLI.process(config, MOCK_TEMPLATE)
-    res.should include(:ip => '5.6.7.8')
-  end
-
-  it 'should override vCloud zone defaults with machine template options' do
-    config = { :foo => { :ip => '1.2.3.4' } }
-    tpl = MOCK_TEMPLATE.dup
-    tpl[:ip] = '5.6.7.8'
-    res = Gds::Provisioner::CLI.process(config, tpl)
-    res.should include(:ip => '5.6.7.8')
-  end
-
-  it 'should override machine template options with command line options' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl[:ssh_user] = 'aaron'
-    opts = { :ssh_user => 'binky' }
-    res = Gds::Provisioner::CLI.process({}, MOCK_TEMPLATE, opts)
-    res.should include(:ssh_user => 'binky')
-  end
-
-  it 'should use catalog_id if present' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl.merge!(
-      :catalog_id => 'wibble',
-      :catalog_items => { :my_cool_template => 'incorrect' },
-      :template_name => 'my_cool_template'
-    )
-    res = Gds::Provisioner::CLI.process({}, tpl)
-    res.should include(:catalog_id => 'wibble')
-  end
-
-  it 'should use template_name to determine catalog_id if catalog_id not present' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl.delete(:catalog_id)
-    tpl.merge!(
-      :catalog_items => { :my_cool_template => 'https://correct_catalog_id' },
-      :template_name => 'my_cool_template'
-    )
-    res = Gds::Provisioner::CLI.process({}, tpl)
-    res.should include(:catalog_id => 'https://correct_catalog_id')
-  end
-
-  it 'should require a zone to be set' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl.delete(:zone)
-    expect do
-      Gds::Provisioner::CLI.process({}, tpl)
-    end.to raise_error(Gds::Provisioner::ConfigurationError, /zone/)
-  end
-
-  it 'should require a puppetmaster to be set' do
-    tpl = MOCK_TEMPLATE.dup
-    tpl.delete(:puppetmaster)
-    expect do
-      Gds::Provisioner::CLI.process({}, tpl)
-    end.to raise_error(Gds::Provisioner::ConfigurationError, /puppetmaster/)
+    it "should fail if two arguments not provided" do
+      expect { Gds::Provisioner::CLI.new(['file1']).execute }.to raise_error(SystemExit)
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,20 @@ $:.unshift File.join(specdir, '..')
 
 require 'rspec'
 
+# Redirects stderr and stdout to /dev/null.
+def silence_output
+  @orig_stderr = $stderr
+  @orig_stdout = $stdout
+
+  # redirect stderr and stdout to /dev/null
+  $stderr = File.new('/dev/null', 'w')
+  $stdout = File.new('/dev/null', 'w')
+end
+
+# Replace stdout and stderr so anything else is output correctly.
+def enable_output
+  $stderr = @orig_stderr
+  $stdout = @orig_stdout
+  @orig_stderr = nil
+  @orig_stdout = nil
+end
