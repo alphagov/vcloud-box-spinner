a8563f39ed41f97d80da86ee301bc0b4edbb336c
Garima Singh <igarimasingh@gmail.com>
2013-08-08 12:12:44 +0100

    Adding tests and test holders for delete vApp functionality

--- spec/provisioner/provisioner_spec.rb
+++ spec/provisioner/provisioner_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 require 'provisioner/errors'
 require 'provisioner/provisioner'
 
-describe 'Provisioner::Provisioner' do
-
-  it "should error if credentials not provided" do
+shared_examples "validate credentials" do |action|
+  it "should error if credentials not provided while perfoming #{action}" do
     logger = mock(:debug => true, :error => true)
     expect {
-      Provisioner::Provisioner.new({:logger => logger}).execute('create')
+      Provisioner::Provisioner.new({:logger => logger}).execute(action)
     }.to raise_error(Provisioner::ConfigurationError,
                      'VCloud credentials must be specified')
   end
+end
+
+describe 'Provisioner::Provisioner' do
+
+  it_should_behave_like "validate credentials", 'create'
+  it_should_behave_like "validate credentials", 'delete'
 
   it "should error out if action passed isn't present" do
     logger = mock(:debug => true, :error => true)
@@ -18,6 +23,12 @@ describe 'Provisioner::Provisioner' do
       Provisioner::Provisioner.new({:logger => logger}).execute('disco')
     }.to raise_error(Provisioner::ConfigurationError,
                      'The action \'disco\' is not a valid action')
+  end
+
+  describe "delete" do
+    pending "should delete vApp for the given vm name" do
+
+    end
   end
 
 end
